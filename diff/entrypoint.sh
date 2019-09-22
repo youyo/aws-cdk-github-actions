56ef3132441401f1f2e34c458b5800ca03ed6387
youyo <1003ni2@gmail.com>
2019-09-23 02:49:33 +0900

    update

--- diff/entrypoint.sh
+++ diff/entrypoint.sh
@@ -7,15 +7,15 @@ wrap() {
 	if [[ $(echo "$1" | wc -l) -gt ${WRAP_LINES:-20} ]]; then
 		echo "
 <details><summary>Show Output</summary>
-\`\`\`diff
-$1
+\`\`\`
+${1}
 \`\`\`
 </details>
 "
 	else
 		echo "
-\`\`\`diff
-$1
+\`\`\`
+${1}
 \`\`\`
 "
 	fi
@@ -25,14 +25,14 @@ if [ -e requirements.txt ]; then
 	pip install -r requirements.txt
 fi
 
-OUTPUT="$(sh -c "cdk diff" 2>&1 ; true)"
+OUTPUT=$(sh -c "cdk diff $*" 2>&1 ; true)
 echo "${OUTPUT}"
 
-COMMENT="#### \`cdk diff\`
-$(wrap "$OUTPUT")
+COMMENT="#### \`cdk diff $*\`
+$(wrap "${OUTPUT}")
 
 *Workflow: \`$GITHUB_WORKFLOW\`, Action: \`$GITHUB_ACTION\`*"
 
-PAYLOAD="$(echo '{}' | jq --arg body "${COMMENT}" '.body = $body')"
-COMMENTS_URL="$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)"
+PAYLOAD=$(echo '{}' | jq --arg body "${COMMENT}" '.body = $body')
+COMMENTS_URL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
 curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${PAYLOAD}" "${COMMENTS_URL}" > /dev/null
