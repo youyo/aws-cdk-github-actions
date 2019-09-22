3c5db8437651c4a250dbc45ea507d94abd77a543
youyo <1003ni2@gmail.com>
2019-09-23 04:19:52 +0900

    update

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -30,11 +30,12 @@ if [ -e requirements.txt ]; then
 	echo ''
 fi
 
-echo 'Run cdk diff'
-OUTPUT=$(sh -c "cdk diff $*" 2>&1)
+echo 'Run cdk deploy'
+OUTPUT=$(sh -c "cdk deploy --require-approval never $*" 2>&1)
+SUCCESS=$?
 echo "${OUTPUT}"
 
-COMMENT="#### \`cdk diff $*\`
+COMMENT="#### \`cdk deploy --require-approval never $*\`
 $(wrap "${OUTPUT}")
 
 *Workflow: \`$GITHUB_WORKFLOW\`, Action: \`$GITHUB_ACTION\`*"
@@ -44,3 +45,5 @@ COMMENTS_URL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
 if [ "${COMMENTS_URL}" != "null" ]; then
 	curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${PAYLOAD}" "${COMMENTS_URL}" > /dev/null
 fi
+
+exit ${SUCCESS}
