3142d75bd2ea32b71e33b9e0e204de5f30cf2b82
youyo <1003ni2@gmail.com>
2019-09-23 02:03:26 +0900

    WIP

---
+++ diff/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -u
+
+# wrap takes some output and wraps it in a collapsible markdown section if it's over $WRAP_LINES long.
+wrap() {
+	if [[ $(echo "$1" | wc -l) -gt ${WRAP_LINES:-20} ]]; then
+		echo "
+<details><summary>Show Output</summary>
+\`\`\`diff
+$1
+\`\`\`
+</details>
+"
+	else
+		echo "
+\`\`\`diff
+$1
+\`\`\`
+"
+	fi
+}
+
+OUTPUT="$(sh -c "cdk diff" 2>&1 ; true)"
+echo "${OUTPUT}"
+COMMENT="#### \`cdk diff\` Success
+${OUTPUT}
+
+*Workflow: \`$GITHUB_WORKFLOW\`, Action: \`$GITHUB_ACTION\`*"
+
+PAYLOAD="$(echo '{}' | jq --arg body "${COMMENT}" '.body = $body')"
+COMMENTS_URL="$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)"
+curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${PAYLOAD}" "${COMMENTS_URL}" > /dev/null
