701950d6afc786655463d8b69b74798e71c66dfb
Cemal Y. Dalar <cemal@dalar.net>
2022-01-03 14:42:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,7 +63,7 @@ function installPipRequirements(){
 function runCdk(){
 	echo "Run cdk ${INPUT_CDK_SUBCOMMAND} ${*} \"${INPUT_CDK_STACK}\""
 	set -o pipefail
-	cdk ${INPUT_CDK_SUBCOMMAND} ${*} "${INPUT_CDK_STACK}" 2>&1 | tee output.log
+	cdk ${INPUT_CDK_SUBCOMMAND} ${*} ${INPUT_CDK_STACK} 2>&1 | tee output.log
 	exitCode=${?}
 	set +o pipefail
 	echo ::set-output name=status_code::${exitCode}
